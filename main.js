faca1973627df93938f7319dd1ac26d820e75c37
Irving SD <irvingjordy@gmail.com>
2025-05-05 13:35:32 -0600

    añadiendo sonidos a cada tecla con JS

---
+++ main.js
@@ -0,0 +1,20 @@
+function playSonido (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0 ;
+
+while (contador < 9) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    console.log(instrumento)
+
+    const idAudio = `#sonido_${instrumento}`;
+    console.log(idAudio)
+
+    tecla.onclick = function (){
+        playSonido(idAudio)
+    };
+    contador ++;
+}
