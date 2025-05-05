164a75eafd82d0a4bbea3b9ba993315df1bc6b19
Irving SD <irvingjordy@gmail.com>
2025-05-05 13:46:21 -0600

    optimixando con el uso de for

--- main.js
+++ main.js
@@ -3,18 +3,16 @@ function playSonido (idElementoAudio) {
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0 ;
 
-while (contador < 9) {
+
+for (let contador=0; contador<listaDeTeclas.length; contador++){
+    
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    console.log(instrumento)
 
     const idAudio = `#sonido_${instrumento}`;
-    console.log(idAudio)
 
     tecla.onclick = function (){
         playSonido(idAudio)
     };
-    contador ++;
 }
